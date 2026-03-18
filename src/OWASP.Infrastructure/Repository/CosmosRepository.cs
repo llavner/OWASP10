8a337ec0c833f687960226cbbf39721393fb4e17
llavner <marcus.renvall@gmail.com>
2026-03-18 14:02:20 +0100

    fixed interface and made services public

--- src/OWASP.Infrastructure/Repository/CosmosRepository.cs
+++ src/OWASP.Infrastructure/Repository/CosmosRepository.cs
@@ -7,9 +7,9 @@ public class CosmosRepository : IOvertimeEntryRepository
 {
     public Task AddAsync(OvertimeEntry entry) => throw new NotImplementedException();
 
-    public Task DeleteAsync(long id) => throw new NotImplementedException();
+    public Task DeleteAsync(Guid userId, long id) => throw new NotImplementedException();
 
-    public Task<OvertimeEntry> GetByIdAsync(long id) => throw new NotImplementedException();
+    public Task<OvertimeEntry?> GetByIdAsync(Guid userId, long id) => throw new NotImplementedException();
 
     public Task<IReadOnlyList<OvertimeEntry>> SearchAsync(Guid userId, DateOnly from, DateOnly to) => throw new NotImplementedException();
 
