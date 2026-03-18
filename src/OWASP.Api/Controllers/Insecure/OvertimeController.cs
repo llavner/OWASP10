709f8ea3d69361aec3438d7d05bd82722892a1c3
llavner <marcus.renvall@gmail.com>
2026-03-18 14:02:34 +0100

    added insecure and secure hollow controllers

---
+++ src/OWASP.Api/Controllers/Insecure/OvertimeController.cs
@@ -0,0 +1,42 @@
+namespace OWASP.Api.Controllers.Insecure;
+
+using Microsoft.AspNetCore.Mvc;
+using OWASP.Api.Attributes;
+
+[Route("api/insecure/[controller]")]
+[ApiController]
+[RequireInsecureDemosEnabled]
+public class OvertimeController : ControllerBase
+{
+    // GET: api/<OvertimeController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<OvertimeController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<OvertimeController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<OvertimeController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<OvertimeController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
