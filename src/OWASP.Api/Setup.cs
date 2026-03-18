c979583f3466a38c813d7d6c77c950d13283b5a8
llavner <marcus.renvall@gmail.com>
2026-03-18 10:04:43 +0100

    added references to the infrastructure project and setup DI for InMeory db

--- src/OWASP.Api/Setup.cs
+++ src/OWASP.Api/Setup.cs
@@ -2,11 +2,13 @@ namespace OWASP.Api;
 
 using OWASP.Api.Auth;
 using OWASP.Application.Interfaces;
+using OWASP.Infrastructure.Repository;
 
 public static class Setup
 {
     public static void AddApiServices(this IServiceCollection services)
     {
         services.AddScoped<ICurrentUserAccessor, StubCurrentUserAccessor>();
+        services.AddSingleton<IOvertimeEntryRepository, InMemoryRepository>();
     }
 }
--- src/OWASP.Infrastructure/Repository/InMemoryRepository.cs
+++ src/OWASP.Infrastructure/Repository/InMemoryRepository.cs
@@ -1,10 +1,14 @@
 namespace OWASP.Infrastructure.Repository;
 
+using System.Collections.Concurrent;
+
 using OWASP.Application.Interfaces;
 using OWASP.Domain.Models;
 
 public class InMemoryRepository : IOvertimeEntryRepository
 {
+    private readonly ConcurrentDictionary<Guid, ConcurrentDictionary<int, OvertimeEntry>> data = new();
+
     public Task AddAsync(OvertimeEntry entry) => throw new NotImplementedException();
 
     public Task DeleteAsync(long id) => throw new NotImplementedException();
