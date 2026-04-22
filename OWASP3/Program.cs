f23c8fb2577a6a68112cda4a4c2b7de9d9ef33f1
llavner <marcus.renvall@gmail.com>
2026-04-22 07:57:16 +0200

    added swashbuckle

--- OWASP3/Program.cs
+++ OWASP3/Program.cs
@@ -1,12 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
