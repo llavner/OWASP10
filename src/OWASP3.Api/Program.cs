01c277d1b0a57102df0311b90279d92857b2a312
Marcus Renvall <157025755+llavner@users.noreply.github.com>
2026-04-22 09:43:58 +0200

    moved project to correct folder (#25)

--- src/OWASP3.Api/Program.cs
+++ src/OWASP3.Api/Program.cs
@@ -6,10 +6,13 @@ builder.Services.AddControllers();
 
 builder.Services.AddAuthenticationServices();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
