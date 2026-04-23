645a0d00424257e45a1baa31645e1e84b95715d5
llavner <marcus.renvall@gmail.com>
2026-04-23 17:21:23 +0200

    initial webpage commit

---
+++ src/OWASP.Gui/Program.cs
@@ -0,0 +1,28 @@
+using OWASP.Gui;
+using OWASP.Gui.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddKeyVaultSecrets();
+builder.AddCosmosDb();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
