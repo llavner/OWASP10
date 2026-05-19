dc6d2702be77feed6d8ec64b7aacaf0eb60bf9d6
Marcus Renvall <157025755+llavner@users.noreply.github.com>
2026-05-19 08:16:02 +0200

    refactor(infrastructure): made repository filter before service (#40)

--- src/OWASP.Application/Interfaces/IOvertimeEntryService.cs
+++ src/OWASP.Application/Interfaces/IOvertimeEntryService.cs
@@ -5,8 +5,6 @@ using OWASP.Domain.Models;
 
 public interface IOvertimeEntryService
 {
-    Task<OvertimeEntry?> GetEntryByIdAsync(string userId, OvertimeEntryRequest request);
-
     Task<List<OvertimeEntry>> GetAllEntriesAsync(string userId);
 
     Task<string> AddEntryAsync(string userId, OvertimeEntryRequest request);
