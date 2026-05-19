722e759823bc9e0c8d26203d30fb216238495156
llavner <marcus.renvall@gmail.com>
2026-05-19 12:56:30 +0200

    feat(core): added basic resulkt pattern to overtimeservice

--- src/OWASP.Application/Interfaces/IOvertimeEntryService.cs
+++ src/OWASP.Application/Interfaces/IOvertimeEntryService.cs
@@ -1,11 +1,14 @@
 namespace OWASP.Application.Interfaces;
 
+using OWASP.Application.Common;
 using OWASP.Application.Dtos;
 using OWASP.Domain.Models;
 
+using static OWASP.Application.Common.OvertimeEntryResultCodes;
+
 public interface IOvertimeEntryService
 {
-    Task<List<OvertimeEntry>> GetAllEntriesAsync(string userId);
+    Task<Result<List<OvertimeEntry>, OvertimeEntryResultCode>> GetAllEntriesAsync(string userId);
 
-    Task AddEntryAsync(string userId, OvertimeEntryRequest request);
+    Task<Result<OvertimeEntry, OvertimeEntryResultCode>> AddEntryAsync(string userId, OvertimeEntryRequest request);
 }
